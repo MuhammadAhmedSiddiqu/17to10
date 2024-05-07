9b43bd0c738ad19c2abb7d348efbafd086245c9c
MuhammadAhmedSiddiqu <ahmedsiddiqui461@gmail.com>
2024-05-07 09:46:54 +0500

    all filew

---
+++ app.js
@@ -0,0 +1,124 @@
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+var array = ["",""]
+
+
+// 2. Declare and initialize a multidimensional array
+// representing the following matrix:
+
+
+// let matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]];
+  
+ 
+//   document.write(matrix[0]+ "<br>" ); 
+//   document.write(matrix[1]+ "<br>"); 
+//   document.write(matrix[2]+ "<br>"); 
+  
+
+// 3. Write a program to print numeric counting from 1 to 10.
+
+// for (i = 1 ; i <= 10 ; i++ ) {
+//     document.write(i + "<br>")
+// }
+
+// 4. Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user.
+
+// var tableNum = +prompt("enter any nuber")
+// var kahatk = +prompt("kaha tk chahiye bhai")
+
+// for ( i = 1 ; i <= kahatk; i++){
+//    document.write( tableNum + "*" + i + "=" +  tableNum * i + "<br>"  )
+// }
+
+// 5. Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+// var fruits  = ["apple", "banana", "mango", "orange","strawberry"]
+
+// for (i = 0 ; i < fruits.length; i++){
+//     document.write(      i +fruits[i] + "<br>")
+// // }
+
+// 6. Generate the following series in your browser. See
+// example output.
+// a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+// c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+// d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+// e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+
+// A) 
+// for (i = 1 ; i <= 15 ; i++){
+//     document.write(i + " ")
+// }
+
+//  b)
+//   for (i = 10 ; i >= 1 ; i--){
+//      document.write(i + " ")
+//  }
+
+// C)
+
+// for (i = 0 ; i <= 20 ; i=i+2){
+//          document.write((i ) +"<br>")
+//      }
+
+// D)
+// for (i = 1 ; i <= 19 ; i=i+2){
+//     document.write((i ) +"<br>")
+// }
+
+// E)
+// for (i = 0 ; i <= 20 ; i=i+2){
+//              document.write((i ) + "K" +"<br>")
+//           }
+
+
+// 7. You have an array
+// A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”]
+// Write a program to enable “search by user input” in an
+// array.
+// After searching, prompt the user whether the given item is
+// found in the list or not. Example:
+
+// var pataBhai = prompt("kuch bhhok" )
+
+// var bakeryItem = ["cake", "apple pie", "cookie", "chips", "patties"]
+// var match = false ;
+// for (i=0 ; i < bakeryItem.length; i++ ){
+//     if(pataBhai === bakeryItem[i]){
+//         match=true
+//         alert("available") 
+//         break
+//     } 
+    
+    
+    
+// }
+
+
+
+// if ( !match) {
+//    alert ("not avalable")
+// }]
+// 8. Write a program to identify the largest number in the
+// given array.
+// A
+// var array = [24, 53, 78, 12];
+
+// var uthaa = array[0]
+
+// for ( i = 0 ; i < array.length ; i++){
+//     if ( uthaa < array [i] ) {
+//         uthaa=(array[i])
+//            }
+
+//         }
+        
+        
+//         document.write(uthaa)
